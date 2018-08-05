28dbd272d4073a4d64c07b8addcb913c321767b5
Yury Kornilov <kornilovyv81@gmail.com>
2018-08-05 23:15:06 +0300

    added silverlightserver

---
+++ src/server/silverlightserver/index.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const net = require('net');
+const { clientAccessPolicyFile, port } = require('./config');
+
+const pathName = path.join(__dirname, clientAccessPolicyFile);
+const policy = fs.readFileSync(pathName, 'utf-8');
+
+function createServer(logger = console) {
+  const server = net.createServer((socket) => {
+    logger.log('event', `client connected to silverlightserver: ${socket.remoteAddress}`);
+
+    socket.on('data', (data) => {
+      if (data.toString().trim() === '<policy-file-request/>') {
+        socket.write(policy);
+      }
+      socket.end();
+    });
+
+    socket.on('error', (err) => {
+      logger.log('alarm', `silverlightclient error: ${err.message}`);
+    });
+
+    socket.on('close', () => {
+      logger.log('event', `silverlighclient closed connection: ${socket.remoteAddress}`);
+    });
+  });
+
+  server.on('listening', () => {
+    logger.log('event', `silverlightserver listening on port ${port}`);
+  });
+
+  server.on('error', (err) => {
+    logger.log('alarm', `silverlightserver error: ${err.message}`);
+    server.close();
+    setTimeout(() => server.listen(port), 1000);
+  });
+
+  server.listen(port);
+
+  return server;
+}
+
+module.exports = {
+  createServer,
+};
