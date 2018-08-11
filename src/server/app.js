dc1f43fa9311e4ed0c49a0ab2a88702ff2169986
Yury Kornilov <kornilovyv81@gmail.com>
2018-08-12 00:47:26 +0300

    modified main file

--- src/server/app.js
+++ src/server/app.js
@@ -1,1 +1,14 @@
-const { getArg } = require('./arguments');
+// const { getArg } = require('./arguments');
+// const { logger } = require('./logger');
+const config = require('./mainconfig');
+const silverlight = require('./silverlightserver');
+const { BeckhoffClient } = require('./beckhoff');
+const { DataServer } = require('./dataserver');
+const { Dispatcher } = require('./dispatcher');
+
+if (config.silverlightClient) {
+  silverlight.createServer();
+}
+
+const dispatcher = new Dispatcher(new BeckhoffClient(), new DataServer());
+dispatcher.start();
