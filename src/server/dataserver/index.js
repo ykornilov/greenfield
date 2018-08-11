3a500766ceb29a2e2d303e4b96cc57d574393389
Yury Kornilov <kornilovyv81@gmail.com>
2018-08-12 00:03:53 +0300

    modified dataserver

--- src/server/dataserver/index.js
+++ src/server/dataserver/index.js
@@ -1,38 +1,107 @@
 const net = require('net');
+const events = require('events');
 const { port } = require('./config');
 
-function createServer(logger = console) {
-  const server = net.createServer((socket) => {
-    logger.log('event', `client connected to dataserver: ${socket.remoteAddress}`);
+class DataServer extends events.EventEmitter {
+  costructor(logger = console) {
+    super();
+    this.logger = logger;
+    this.server = null;
+    this.clients = [];
+  }
 
-    socket.on('data', (data) => {
-      logger.log(data.toString());
+  start() {
+    this.server = net.createServer((socket) => {
+      this.logger.log('event', `client connected to dataserver: ${socket.remoteAddress}`);
+      this.clients.push(socket);
+
+      socket.on('data', (data) => {
+        this.emit('data', data);
+      });
+
+      socket.on('error', (err) => {
+        this.logger.log('event', `dataclient error: ${err.message}`);
+        this.destroyClient(socket);
+      });
+
+      socket.on('close', () => {
+        this.logger.log('event', `dataclient closed connection: ${socket.remoteAddress}`);
+        this.destroyClient(socket);
+      });
     });
 
-    socket.on('error', (err) => {
-      logger.log('event', `dataclient error: ${err.message}`);
+    this.server.on('listening', () => {
+      this.logger.log('event', `dataserver listening on port ${port}`);
     });
 
-    socket.on('close', () => {
-      logger.log('event', `dataclient closed connection: ${socket.remoteAddress}`);
+    this.server.on('error', (err) => {
+      this.logger.log('event', `dataserver error: ${err.message}`);
+      this.restart(1000);
     });
-  });
 
-  server.on('listening', () => {
-    logger.log('event', `dataserver listening on port ${port}`);
-  });
+    this.server.listen(port);
+  }
 
-  server.on('error', (err) => {
-    logger.log('event', `dataserver error: ${err.message}`);
-    server.close();
-    setTimeout(() => server.listen(port), 1000);
-  });
+  destroyClient(socket) {
+    this.clients = this.clients.filter(client => client !== socket);
+    socket.destroy();
+  }
 
-  server.listen(port);
+  send(data) {
+    this.clients.forEach(client => client.write(data, 'utf-8', (err) => {
+      if (err) {
+        this.logger.log('event', `dataclient send error: ${err.message}`);
+        this.destroyClient(client);
+      }
+    }));
+  }
 
-  return server;
+  restart(timeout) {
+    this.stop();
+    setTimeout(() => this.server.listen(port), timeout);
+  }
+
+  stop() {
+    this.clients.forEach(client => client.end());
+    this.clients = [];
+    this.server.removeAllListeners();
+    this.server.close();
+    this.logger.log('event', 'server is stopped');
+  }
 }
 
+// function createServer(logger = console) {
+//   const server = net.createServer((socket) => {
+//     logger.log('event', `client connected to dataserver: ${socket.remoteAddress}`);
+
+//     socket.on('data', (data) => {
+//       logger.log(data.toString());
+//     });
+
+//     socket.on('error', (err) => {
+//       logger.log('event', `dataclient error: ${err.message}`);
+//     });
+
+//     socket.on('close', () => {
+//       logger.log('event', `dataclient closed connection: ${socket.remoteAddress}`);
+//     });
+//   });
+
+//   server.on('listening', () => {
+//     logger.log('event', `dataserver listening on port ${port}`);
+//   });
+
+//   server.on('error', (err) => {
+//     logger.log('event', `dataserver error: ${err.message}`);
+//     server.close();
+//     setTimeout(() => server.listen(port), 1000);
+//   });
+
+//   server.listen(port);
+
+//   return server;
+// }
+
 module.exports = {
-  createServer,
+  DataServer,
 };
